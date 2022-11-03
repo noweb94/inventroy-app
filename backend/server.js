9e67f1a4796c3517e39b2c5983dcdc5fd6454b34
noweb94 <domanglad@email.com>
2022-11-03 12:07:01 +0300

    PAsswords

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,32 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const userRoute = require("./routes/userRoute");
+const errorHandler = require("./middleWare/errorMiddleWare");
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 5000;
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+//Routes middlewares
+app.use("/api/users", userRoute);
+
+//Routes
+app.get("/", (req, res) => {
+    res.send("Home Page");
+});
+
+// Error Middleware
+app.use(errorHandler);
 //connect to DB and start server
+const PORT = process.env.PORT || 5000;
 mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {
-        
+
         app.listen(PORT, () => {
             console.log(`Server Running on Port ${PORT}`)
         })
